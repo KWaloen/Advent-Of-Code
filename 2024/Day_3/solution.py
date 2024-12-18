f11266b4c2d865af58e2685f89bf8e1044543ee6
KWaloen <kai.waloen@gmail.com>
2024-12-18 15:13:37 +0100

    solution day 3

--- 2024/Day_3/solution.py
+++ 2024/Day_3/solution.py
@@ -1,18 +1,53 @@
+
+#part 1
+
+# import re
+
+# products = []
+
+# with open("2024/Day_3/input","r") as file:
+#     text = file.read()
+
+#     pattern = r"mul\((\d+),(\d+)\)"
+
+#     matches = re.findall(pattern, text)
+
+#     for pair in matches:
+#         x, y = map(int, pair)
+#         product = x * y
+#         products.append(product)
+        
+
+# print(sum(products))
+
+#part 2
+
+
+
 import re
 
-products = []
-
-with open("2024/Day_3/input","r") as file:
+with open("2024/Day_3/input", "r") as file:
     text = file.read()
 
-    pattern = r"mul\((\d+),(\d+)\)"
+mul_pattern = r"mul\((\d+),(\d+)\)"
+do_pattern = r"do\(\)"
+dont_pattern = r"don't\(\)"
 
-    matches = re.findall(pattern, text)
+activate = True
+products = []
 
-    for pair in matches:
-        x, y = map(int, pair)
-        product = x * y
-        products.append(product)
-        
+for pattern in re.finditer(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)", text):
+    instruction = pattern.group(0)
+
+    if re.match(do_pattern, instruction):
+        activate = True
+    elif re.match(dont_pattern, instruction):
+        activate = False
+    elif re.match(mul_pattern, instruction):
+        if activate:
+            x,y = map(int, re.findall(r"\d+", instruction))
+            product = x*y
+            print(product)
+            products.append(product)
 
 print(sum(products))
